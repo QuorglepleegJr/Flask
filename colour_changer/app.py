90139e5b995bf7f94576e71bebd9c268496dc318
Benjamin <17wilsb@olab.org.uk>
2023-05-17 12:22:40 +0100

    Finished colour changer

--- colour_changer/app.py
+++ colour_changer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 
@@ -35,17 +35,18 @@ class DemoColourForm(FlaskForm):
         ("amber", "Amber"),
         ("orange", "Orange"),
         ("deep-orange", "Deep Orange"),
-        #("brown", "Brown"),
-        #("grey", "Grey"),
-        #("blue-grey", "Blue Grey"),
-        #("black", "Black"),
-        #("white", "White"),
-        #("transparent", "Transparent"),
+        ("brown", "Brown"),
+        ("grey", "Grey"),
+        ("blue-grey", "Blue Grey"),
+        ("black", "Black"),
+        ("white", "White"),
+        ("transparent", "Transparent"),
         ])
 
 class FullColourForm(DemoColourForm):
 
     modify = SelectField("Modification", choices=[
+        ("", "None"),
         ("lighten", "Lighten"),
         ("darken", "Darken"),
         ("accent", "Accent"),
@@ -75,7 +76,7 @@ def login():
 
         if (username, password) in VALID_LOGINS:
 
-            return redirect(url_for("full_access"))
+            return redirect(url_for("full_access", username=username))
 
         return render_template("login.html", form=login_form, login_error="Invalid login.")
 
@@ -86,4 +87,48 @@ def demo_access():
 
     demo_colour = DemoColourForm()
 
-    return render_template("demo_colour.html", form = demo_colour)
+    colour = "transparent"
+
+    if demo_colour.is_submitted():
+
+        colour = demo_colour.base.data
+
+    return render_template("demo_colour.html", form=demo_colour, colour=colour)
+
+@app.route('/full', methods=['GET','POST'])
+def full_access():
+
+    try:
+
+        username = request.args['username']
+
+        if username not in [user[0] for user in VALID_LOGINS]:
+
+            return redirect(url_for('login'))
+    
+    except KeyError:
+
+        return redirect(url_for('login'))
+    
+    else:
+
+        full_colour = FullColourForm()
+
+        colour = "transparent"
+
+        if full_colour.is_submitted():
+
+            if full_colour.modify.data == "" or full_colour.base.data in ["black","white","transparent"]:
+                
+                colour = full_colour.base.data
+            
+            elif full_colour.base.data in ["brown","grey","blue-grey"] and full_colour.modify.data == "accent":
+
+                colour = full_colour.base.data
+
+            else:
+
+                colour = f"{full_colour.base.data} {full_colour.modify.data}-{full_colour.strength.data}"
+            
+
+        return render_template("full_colour.html", form=full_colour, colour=colour, username=username)
